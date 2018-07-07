330e2aba30cc928ea93942c283869112e4452fbc
César Andrés Parra Rojas <parra@fias.uni-frankfurt.de>
2018-07-07 17:42:59 +0200

    working w/latest retinanet

--- find_wally.py
+++ find_wally.py
@@ -6,7 +6,7 @@ import sys
 import random
 import tensorflow as tf
 import keras
-from keras_retinanet.models.resnet import custom_objects
+from keras_retinanet import models
 from keras_retinanet.utils.image import read_image_bgr, preprocess_image, resize_image
 
 def get_session():
@@ -19,7 +19,7 @@ keras.backend.tensorflow_backend.set_session(get_session())
 sys_args = sys.argv
 
 model_path = sys_args[1]
-model = keras.models.load_model(model_path, custom_objects=custom_objects)
+model = models.load_model(model_path, backbone_name='resnet50')
 
 if len(sys_args)>2:
     image_files=sys.argv[2:]
@@ -35,11 +35,9 @@ for image in image_files:
     image = preprocess_image(image)
     image, scale = resize_image(image, min_side=1800, max_side=3000)
 
-    _, _, detections = model.predict_on_batch(np.expand_dims(image, axis=0))
+    boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))
 
-    scores = detections[0, np.arange(detections.shape[1]), 4]
-
-    detections[0, :, :4] /= scale
+    boxes /= scale
 
     fig = plt.figure()
     ax = plt.Axes(fig, [0., 0., 1., 1.])
@@ -47,13 +45,14 @@ for image in image_files:
 
     img_mask = 255*np.ones_like(draw)
 
-    for idx, score in enumerate(scores):
+    for box, score, label in zip(boxes[0], scores[0], labels[0]):
         if score < 0.5:
-            continue
-        box = detections[0, idx, :4].astype(int)
-        box += np.array([-(box[2] - box[0])//4, -(box[3] - box[1])//4, (box[2] - box[0])//4, (box[3] - box[1])//4])
+            break
 
-        img_mask[box[1]:box[3],box[0]:box[2],:] = draw[box[1]:box[3],box[0]:box[2],:]
+        b = box.astype(int)
+        b += np.array([-(b[2] - b[0])//4, -(b[3] - b[1])//4, (b[2] - b[0])//4, (b[3] - b[1])//4])
+
+        img_mask[b[1]:b[3],b[0]:b[2],:] = draw[b[1]:b[3],b[0]:b[2],:]
 
     ax.imshow(draw, alpha=0.5)
     ax.imshow(img_mask, alpha=0.5)
